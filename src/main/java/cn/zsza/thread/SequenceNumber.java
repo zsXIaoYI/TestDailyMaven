0fbcde11e47688e3c49d69707cb5b8760e70ba71
zhangsong <shanchong@maizijf.com>
2017-06-05 14:25:34 +0800

    新增一个类

---
+++ src/main/java/cn/zsza/thread/SequenceNumber.java
@@ -0,0 +1,40 @@
+package cn.zsza.thread;
+
+/**
+ * Created by zs on 2017/6/1.
+ * 14:26
+ */
+public class SequenceNumber {
+
+    private static ThreadLocal<Integer> seqNum = ThreadLocal.withInitial(() -> 0);
+
+    public int getNextNum(){
+        seqNum.set(seqNum.get() + 1);
+        return seqNum.get();
+    }
+    private static class MyRunnable extends Thread{
+        private SequenceNumber sn;
+
+        public MyRunnable(SequenceNumber sn) {
+            this.sn = sn;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0;i < 3; i++){
+                System.out.println("线程:" + Thread.currentThread().getName() + " sn: " + sn.getNextNum());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SequenceNumber sn = new SequenceNumber();
+
+        MyRunnable m1 = new MyRunnable(sn);
+        MyRunnable m2 = new MyRunnable(sn);
+
+        m1.start();
+        m2.start();
+    }
+
+}
