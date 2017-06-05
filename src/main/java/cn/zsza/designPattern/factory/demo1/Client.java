21b0433e6f96e5728228bb70e268002a859268c7
zhangsong <shanchong@maizijf.com>
2017-06-05 14:32:19 +0800

    修改

--- src/main/java/cn/zsza/designPattern/factory/demo1/Client.java
+++ src/main/java/cn/zsza/designPattern/factory/demo1/Client.java
@@ -1,19 +1,21 @@
 package cn.zsza.designPattern.factory.demo1;
 
-import org.jdom.input.SAXBuilder;
-
 /**
  * Created by zs on 2017/5/15.
  * 16:39
  */
 public class Client {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
         Product product = ProductFactory.getProInstance("A");
         product.methodSame();
         product.methodDiff();
 
-        SAXBuilder builder = new SAXBuilder();
+
+        Class<?> client = Class.forName("cn.zsza.designPattern.factory.demo1.Client");
+        Object obj = client.newInstance();
+        System.out.println("obj:" +obj);
+
 
     }
 }
