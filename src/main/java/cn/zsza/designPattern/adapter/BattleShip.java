7b536640f2eaff4d54a7bb707d89715aece8e4f3
zhangsong <shanchong@maizijf.com>
2017-06-28 18:58:16 +0800

    新增一个类

---
+++ src/main/java/cn/zsza/designPattern/adapter/BattleShip.java
@@ -0,0 +1,12 @@
+package cn.zsza.designPattern.adapter;
+
+/**
+ * Created by zs on 2017/6/28.
+ * 18:48
+ */
+public interface BattleShip {
+
+    void fire();
+
+    void move();
+}
---
+++ src/main/java/cn/zsza/designPattern/adapter/FishingBoat.java
@@ -0,0 +1,16 @@
+package cn.zsza.designPattern.adapter;
+
+/**
+ * Created by zs on 2017/6/28.
+ * 18:49
+ */
+public class FishingBoat {
+
+    public void sail(){
+        System.out.println("The boat is sailing");
+    }
+
+    public void fish(){
+        System.out.println("fishing....");
+    }
+}
