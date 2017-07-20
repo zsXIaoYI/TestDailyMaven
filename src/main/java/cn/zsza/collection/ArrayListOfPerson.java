fe8df24b715903315c40308233537a498f9d782d
zhangsong <shanchong@maizijf.com>
2017-07-20 17:35:30 +0800

    修改

--- src/main/java/cn/zsza/collection/ArrayListOfPerson.java
+++ src/main/java/cn/zsza/collection/ArrayListOfPerson.java
@@ -1,5 +1,7 @@
 package cn.zsza.collection;
 
+import cn.zsza.zs_base.entity.Person;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -7,27 +9,6 @@ import java.util.List;
  * Created by zs on 2017/6/12.
  * 18:19
  */
-class Person{
-    private int idNum;
-    public Person(){
-    }
-    public Person(int idNum) {
-        this.idNum = idNum;
-    }
-    @Override
-    public String toString() {
-        return "Person{" +
-                "idNum=" + idNum +
-                '}';
-    }
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || this.getClass() != o.getClass()) return false;
-        Person person = (Person) o;
-        return idNum == person.idNum;
-    }
-}
 public class ArrayListOfPerson {
     public static void main(String[] args) {
         List<Person> personList = new ArrayList<>();
@@ -35,7 +16,6 @@ public class ArrayListOfPerson {
         personList.add(new Person(2));
         personList.add(new Person(2));
         personList.add(new Person(3));
-//        System.out.println("personList="+personList);
         Iterator<Person> it = personList.iterator();
         while (it.hasNext()){
             Person per = it.next();
