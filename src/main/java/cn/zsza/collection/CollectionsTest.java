a680b94059b7cd7e3d99deb930f900ccded1e64c
zhangsong <shanchong@maizijf.com>
2017-07-20 17:19:03 +0800

    添加代码

--- src/main/java/cn/zsza/collection/CollectionsTest.java
+++ src/main/java/cn/zsza/collection/CollectionsTest.java
@@ -1,33 +1,26 @@
 package cn.zsza.collection;
 
+import cn.zsza.util.StrLenCom;
+import org.junit.Before;
 import org.junit.Test;
 import java.util.*;
 /**
  * Created by user on 2016/1/26.
  * 声明一个String类型的Comparator,其泛型为<String>
  */
-class StrLenCom implements Comparator<String> {
-    @Override
-    public int compare(String o1, String o2) {
-        if (o1.length() > o2.length()){
-            return 1;
-        }
-        if (o1.length() < o2.length()){
-            return -1;
-        }
-        return o1.compareTo(o2);
-    }
-}
+
 public class CollectionsTest {
-    @Test
-    public void testSort(){
-        List<String> list = new ArrayList<>();
+
+    private List<String> list;
+    @Before
+    public void initList(){
         list.add("ddd");
-        list.add("a");
+        list.add("aa");
         list.add("ab");
         list.add("ccff");
-
-        System.out.println("初始化的list:"+list);
+    }
+    @Test
+    public void testSort(){
         // 对List集合进行排序,Collections.sort()对其按照自然顺序进行排序
         Collections.sort(list);
         System.out.println("按自然顺序进行排序："+list);
@@ -39,12 +32,7 @@ public class CollectionsTest {
     }
     @Test
     public void testMax(){
-        List<String> list = new ArrayList<>();
-        list.add("ddd");
-        list.add("a");
-        list.add("ab");
-        list.add("ccff");
-        list.add("zs");
+
 
         // 对List集合进行排序
         Collections.sort(list);
@@ -57,11 +45,6 @@ public class CollectionsTest {
 
     @Test
     public void testReplace(){
-        List<String> list = new ArrayList<>();
-        list.add("ddd");
-        list.add("aa");
-        list.add("ab");
-        list.add("ccff");
         /**
          * 将集合元素"aa"替换成"kk"
          */
@@ -71,10 +54,6 @@ public class CollectionsTest {
 
     @Test
     public void testReverse(){
-        List<String> list = new ArrayList<>();
-        list.add("a");
-        list.add("b");
-        list.add("c");
         /**
          * 集合元素反转
          */
@@ -102,14 +81,20 @@ public class CollectionsTest {
     }
     @Test
     public void testSwap(){
-        List<String> list = new ArrayList<>();
-        list.add("a");
-        list.add("b");
-        list.add("c");
         /**
          * 把集合中位置0和1上的元素进行交换。
          */
         Collections.swap(list,0,1);
         System.out.println(list);
+    }
+
+    @Test
+    public void testEmptyList(){
+        /**
+         * 返回的集合对象是immutable的,不可更改的
+         */
+        List emptyList = Collections.EMPTY_LIST;
+        System.out.println("emptyList:" + emptyList);
+        System.out.println("size:" + emptyList.size());
     }
 }
