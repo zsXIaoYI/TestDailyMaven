71d27af40b401bdb7d0760b628a4d330ef668808
zhangsong <shanchong@maizijf.com>
2017-07-21 18:13:53 +0800

    修改

--- src/main/java/cn/zsza/App.java
+++ src/main/java/cn/zsza/App.java
@@ -5,7 +5,15 @@ package cn.zsza;
  */
 public class App {
     public static void main( String[] args ) {
+        /**
+         * ExtClassLoader需加载的类库
+         * win10 PC测试
+         * ext:C:\Program Files\Java\jdk1.8.0_101\jre\lib\ext;C:\Windows\Sun\Java\lib\ext
+         */
         String ext = System.getProperty("java.ext.dirs");
+        /**
+         * AppClassLoader需加载的类库
+         */
         String classPath = System.getProperty("java.class.path");
         String[] classPaths = classPath.split(";");
 
