0a327152b622dab49c740dedc60530768c95d6a5
zhangsong <shanchong@maizijf.com>
2017-07-25 11:01:02 +0800

    新增一个枚举

---
+++ src/main/java/cn/zsza/util/InviteType.java
@@ -0,0 +1,29 @@
+package cn.zsza.util;
+
+/**
+ * @author shanchong
+ * @create 2017/7/13
+ */
+public enum InviteType {
+
+
+    INVITE("1", "邀请"),
+    INVITED("2","被邀请");
+    private String val;
+    private String name;
+
+    InviteType(String val, String name) {
+        this.val = val;
+        this.name = name;
+    }
+
+    public String value() {
+        return this.val;
+    }
+
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
