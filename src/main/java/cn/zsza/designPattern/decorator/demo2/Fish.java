f48fa4b45f7039be0d3177a0bf8cbb75a61f87c9
zhangsong <shanchong@maizijf.com>
2017-08-10 10:13:04 +0800

    加注释

--- src/main/java/cn/zsza/designPattern/decorator/demo2/Fish.java
+++ src/main/java/cn/zsza/designPattern/decorator/demo2/Fish.java
@@ -4,6 +4,7 @@ package cn.zsza.designPattern.decorator.demo2;
  * Created by zs on 2017/8/9.
  * 18:08
  */
+// 具体装饰角色"鱼儿"
 public class Fish extends Change {
     public Fish(TheGreatestSage sage) {
         super(sage);
