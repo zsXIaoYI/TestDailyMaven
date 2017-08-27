91b2b0bc6a2954b4817eb83f78e79bcc16fa5929
zhangsong <zsza5466@163.com>
2017-08-27 21:36:29 +0800

    新增

---
+++ src/main/java/cn/zsza/spring/service/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package cn.zsza.spring.service.impl;
+
+import cn.zsza.spring.service.UserService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by ZhangSong on 2017/4/30.
+ * 13:58
+ */
+@Service
+public class UserServiceImpl implements UserService {
+    public void testMethod() {
+        System.out.println("userService方法");
+    }
+}
