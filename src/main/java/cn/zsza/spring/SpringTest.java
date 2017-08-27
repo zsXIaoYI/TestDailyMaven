2b4298a61e8b72928a737c7232417d88296ce8e8
zhangsong <zsza5466@163.com>
2017-08-27 17:24:33 +0800

    新增

--- src/main/java/cn/zsza/spring/SpringTest.java
+++ src/main/java/cn/zsza/spring/SpringTest.java
@@ -1,7 +1,8 @@
 package cn.zsza.spring;
 
+import cn.zsza.spring.aspect.PersonService;
 import cn.zsza.spring.dao.PersonDao;
-import cn.zsza.spring.service.PersonService;
+import cn.zsza.spring.service.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -25,14 +26,42 @@ public class SpringTest {
         personDao.print();
     }
     /**
+     * <content:annotation-config/>
      * resource注解
      * 对于service层调用dao层的业务方法,对dao的注入,交由Spring容器处理
      */
     @Test
     public void test2(){
         ApplicationContext ctx = new ClassPathXmlApplicationContext("/Spring/Spring-resource.xml");
-        PersonService personService = (PersonService) ctx.getBean("personService");
+        cn.zsza.spring.service.PersonService personService = (cn.zsza.spring.service.PersonService) ctx.getBean("personService");
         personService.print();
+    }
+
+    /**
+     * resource注解,基于包的自动扫描
+     * <content:component-scan/>
+     */
+    @Test
+    public void test3(){
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("/Spring/Spring-resource-component-scan.xml");
+        UserService userService = (UserService) ctx.getBean("userServiceImpl");
+        userService.testMethod();
+    }
+
+
+    @Test
+    public void testAOP(){
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("/Spring/Spring-aop.xml");
+        PersonDao personDao = (PersonDao) ctx.getBean("personDao");
+        personDao.print();
+    }
+    @Test
+    public void testAOP2(){
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("/Spring/Spring-aop-aspect.xml");
+        PersonService personService = (PersonService) ctx.getBean("personService");
+        // 只有一个String类型参数的前置通知被执行
+        personService.save("小A");
+
     }
 
 }
