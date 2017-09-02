2938a59d523f6e00b7d2c9b96353854d70cd4326
zhangsong <zsza5466@163.com>
2017-09-02 14:42:39 +0800

    修改

--- src/main/java/cn/zsza/spring/SpringTest.java
+++ src/main/java/cn/zsza/spring/SpringTest.java
@@ -2,6 +2,10 @@ package cn.zsza.spring;
 
 import cn.zsza.spring.aspect.PersonService;
 import cn.zsza.spring.dao.PersonDao;
+import cn.zsza.spring.entity.Person;
+import cn.zsza.spring.jdbc.PersonServiceBean_jdbc;
+import cn.zsza.spring.jdbc.PersonService_jdbc;
+import cn.zsza.spring.jdbc.TestTxa;
 import cn.zsza.spring.service.UserService;
 import cn.zsza.spring.zs.ZSClassPathXMLApplicationContext;
 import org.junit.Test;
@@ -66,5 +70,40 @@ public class SpringTest {
     @Test
     public void testMyBeanXML(){
         ZSClassPathXMLApplicationContext ctx = new ZSClassPathXMLApplicationContext("Spring/Spring-ref.xml");
+    }
+    //************************JDBC************************
+    @Test
+    public void testSave(){
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("/Spring/Spring-config-jdbc.xml");
+        PersonService_jdbc personService_jdbc = (PersonService_jdbc) ctx.getBean("personServiceBean");
+        personService_jdbc.save(new Person("小A"));
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("/Spring/Spring-config-jdbc.xml");
+        PersonService_jdbc personService_jdbc = (PersonService_jdbc) ctx.getBean("personServiceBean");
+        personService_jdbc.deletePerson(4);
+
+    }
+
+    @Test
+    public void testTxaNever() throws Exception {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("/Spring/Spring-config-jdbc.xml");
+        TestTxa testTxa = (TestTxa) ctx.getBean("testTxa");
+        testTxa.testNever(10);
+    }
+
+    @Test
+    public void testMANDATORY() throws Exception {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("/Spring/Spring-config-jdbc.xml");
+        TestTxa testTxa = (TestTxa) ctx.getBean("testTxa");
+        testTxa.testMANDATORY(11);
+    }
+    @Test
+    public void testNESTED() throws Exception {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("/Spring/Spring-config-jdbc.xml");
+        TestTxa testTxa = (TestTxa) ctx.getBean("testTxa");
+        testTxa.testNESTED(12,new Person("小B"));
     }
 }
