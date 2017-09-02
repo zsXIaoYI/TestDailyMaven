423045925f3ebe0f70bc0c1496707daf48a22492
zhangsong <zsza5466@163.com>
2017-09-02 12:10:18 +0800

    add

---
+++ src/main/java/cn/zsza/spring/jdbc/PersonService_jdbc.java
@@ -0,0 +1,16 @@
+package cn.zsza.spring.jdbc;
+
+
+import cn.zsza.spring.entity.Person;
+import java.util.List;
+
+/**
+ * Created by user on 2015/9/16.
+ */
+public interface PersonService_jdbc {
+    void save(Person person);
+    void update(Person person);
+    Object getPerson(Integer personId);
+    List<Person> getPersons();
+    void deletePerson(Integer id) throws Exception;
+}
