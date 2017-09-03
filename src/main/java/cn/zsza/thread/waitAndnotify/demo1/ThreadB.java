e78d6caccc46bc0d0153407a0a193d799508f1f9
zhangsong <zsza5466@163.com>
2017-09-03 23:12:02 +0800

    修改

--- src/main/java/cn/zsza/thread/waitAndnotify/demo1/ThreadB.java
+++ src/main/java/cn/zsza/thread/waitAndnotify/demo1/ThreadB.java
@@ -20,11 +20,12 @@ public class ThreadB extends Thread {
             synchronized (lock){
                 for (int i = 0; i < 10; i++){
                     MyList.add();
+                    System.out.println("添加了" + (i + 1) + "个元素");
                     if (MyList.size() == 5){
                         lock.notify();
                         System.out.println("已发出通知...");
+//                        break;
                     }
-                    System.out.println("添加了" + (i + 1) + "个元素");
                     Thread.sleep(1000);
                 }
             }
