b5ac543a5f62f76ec58c391cece3f64fa31bd7b4
zhangsong <zsza5466@163.com>
2017-09-04 20:14:14 +0800

    修改

--- src/main/java/cn/zsza/thread/atomic/SimulatedCAS.java
+++ src/main/java/cn/zsza/thread/atomic/SimulatedCAS.java
@@ -2,8 +2,11 @@ package cn.zsza.thread.atomic;
 
 /**
  * Created by zs on 2017/8/31.
- * 15:12
- * 解释CAS实现过程
+ * 15:16
+ * Compare and swap(CAS)
+ * 当前的处理器基本都支持CAS，只不过每个厂家所实现的算法并不一样罢了，每一个CAS操作过程都包含三个运算符：一个内存地址V，一个期望的值A
+ * 和一个新值B，操作的时候如果这个地址上存放的值等于这个期望的值A，则将地址上的值赋为新值B，否则不做任何操作。CAS的基本思路就是，
+ * 如果这个地址上的值和期望的值相等，则给其赋予新值，否则不做任何事儿，但是要返回原值是多少
  */
 public class SimulatedCAS {
     private int value;
