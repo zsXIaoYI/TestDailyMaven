63d4aaba16acbf1ff5be67727702dade8caecc19
zhangsong <zsza5466@163.com>
2017-09-09 23:01:56 +0800

    新增

---
+++ src/main/java/cn/zsza/zs_base/entity/JMapper.java
@@ -0,0 +1,13 @@
+package cn.zsza.zs_base.entity;
+
+import lombok.Data;
+
+/**
+ * Created by ZhangSong on 2017/9/9.
+ * 21:54
+ */
+@Data
+public class JMapper {
+    private Integer id;
+    private String name;
+}
