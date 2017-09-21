20883555827fad5d07509308eae368da8a7897e1
zhangsong <zsza5466@163.com>
2017-09-21 21:13:01 +0800

    新增

--- src/main/java/cn/zsza/thread/callable/CallableAddSum.java
+++ src/main/java/cn/zsza/thread/callable/CallableAddSum.java
@@ -31,6 +31,7 @@ public class CallableAddSum implements Callable<Integer> {
          * 使用一个基于链表结构的阻塞队列(LinkedBlockingQueue)，此队列安FIFO排序元素，吞吐量通常要高于ArrayBlockingQueue
          */
         ExecutorService pool = Executors.newFixedThreadPool(2);
+        ExecutorService newSingleThreadExecutor = Executors.newSingleThreadExecutor();
         /**
          * 开启两个线程求 1 到 100 的 和
          */
@@ -40,10 +41,14 @@ public class CallableAddSum implements Callable<Integer> {
         
         Integer s1 = f1.get();
         Integer s2 = f2.get();
-
         System.out.println(s1 + s2);
-
         pool.shutdown();
+
+        if (!pool.awaitTermination(10,TimeUnit.NANOSECONDS)){
+            System.out.println("Threads did not finish in 60 NANOSECONDS");
+        }
+        System.out.println(pool.isShutdown());
+        System.out.println(pool.isTerminated());
 
     }
 
