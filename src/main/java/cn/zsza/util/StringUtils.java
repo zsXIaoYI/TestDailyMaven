5f7da7b0e90bba0cddc23efc8bb9b1c3a67e276c
zhangsong <zsza5466@163.com>
2017-09-23 10:52:39 +0800

    修改

--- src/main/java/cn/zsza/util/StringUtils.java
+++ src/main/java/cn/zsza/util/StringUtils.java
@@ -22,4 +22,43 @@ public class StringUtils {
         }
         return true;
     }
+
+    /**
+     * 求一个字符串中最长回文子串
+     * @param str
+     * @return
+     */
+    public static int longestPalindRome(String str){
+        if (str == null || str.length() < 1){
+            return 0;
+        }
+        int max = 0, current = 0, length = str.length();
+        // 循环 每个字符作为回文的中点
+        for (int i = 0; i < length; i++) {
+            // 考虑回文子串长度为奇数的情况
+            for (int j = 0; i - j >=0 && i + j < length ; j++) {
+                if(str.charAt(i - j) != str.charAt(i + j)){
+                    break;
+                }
+                current = j * 2 + 1;
+            }
+            if (current > max){
+                max = current;
+            }
+
+            current = 0;
+            // 考虑回文子串长度为偶数的情况
+            for (int j = 0; (i - j >= 0) && (i + j + 1 < length) ; j++) {
+                if (str.charAt(i - j) != str.charAt(i + j + 1)){
+                    break;
+                }
+                current = j * 2 + 2;
+            }
+            if (current > max){
+                max = current;
+            }
+
+        }
+        return max;
+    }
 }
