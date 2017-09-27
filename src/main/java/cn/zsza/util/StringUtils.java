30153f70776b213100539bde2af5ece5d4c10f61
zhangsong <zsza5466@163.com>
2017-09-27 16:03:10 +0800

    新增

--- src/main/java/cn/zsza/util/StringUtils.java
+++ src/main/java/cn/zsza/util/StringUtils.java
@@ -61,4 +61,123 @@ public class StringUtils {
         }
         return max;
     }
+    public static String addBySum(String s1,String s2){
+        /**
+         * s1 =999 378
+         * s2 =    123
+         */
+        String resultTmp = null;
+        StringBuffer result = new StringBuffer();
+        StringBuffer result3 = new StringBuffer();
+        String r3 = null;
+        int leth1 = s1.length();
+        int leth2 = s2.length();
+        int leth1_2 = leth1 - leth2;
+        char c1[] = s1.toCharArray();
+        char c2[] = s2.toCharArray();
+        int temp = 0;                    // 进位,两数相加和大于10，则往高位进1
+        int res;
+        if (leth1 >= leth2){
+            /**
+             * 先计算s1和s2等长度的两者之间的和
+             */
+            for (int i = leth2 - 1; i >= 0 ; i--) {
+                res = Character.getNumericValue(c1[i + leth1_2]) +
+                        Character.getNumericValue(c2[i]) + temp;
+                String resTmp = "" + res ;
+                if (res >= 10){
+                    temp = 1;    // 往高位进1
+                    result.append(resTmp.charAt(1));
+                    System.out.println();
+                }else {
+                    temp = 0;
+                    result.append(resTmp);
+                }
+            }
+            // 如果前面for循环中有进位，则对其s1.substring(0,leth1 - leth2)计算要+1
+            if (temp > 0){
+                if (leth1 > leth2){
+                    String s3 = s1.substring(0,leth1 - leth2);
+                    char c3[] = s3.toCharArray();
+                    int leth3 = c3.length;
+                    for (int i = leth3 - 1; i >= 0 ; i--) {
+                        int r = Character.getNumericValue(c3[i]) + temp;
+                        String rTmp = "" + r;
+                        if (r >= 10){
+                            temp = 1;  // 往高位进1
+                            result3.append(rTmp.charAt(1));
+                            /**
+                             * 999
+                             *   1
+                             */
+                            if (i == 0){
+                                r3 = "1";
+                            }
+                        }else {
+                            r3 = new String(c3, 0, i);
+                            result3.append(rTmp);
+                            break;
+                        }
+                    }
+                    String finalR3 = r3 + result3.reverse().toString();
+                    resultTmp = finalR3 + result.reverse().toString();
+                }else if (leth1 == leth2){
+                    resultTmp = "1" + result.reverse().toString();
+                }
+            }else {
+                resultTmp = s1.substring(0,leth1 - leth2) + result.reverse().toString();
+            }
+        }
+        return resultTmp;
+    }
+
+    public static String addBySum2(String s1,String s2){
+        String resultTmp;
+        StringBuffer result = new StringBuffer();
+        int leth1 = s1.length();
+        int leth2 = s2.length();
+        char c1[] = s1.toCharArray();
+        char c2[] = s2.toCharArray();
+        int temp = 0;                    // 进位,两数相加和大于10，则往高位进1
+        int res;
+        if (leth1 > leth2){              // 进行补零操作
+            c2 = (getSb0(leth1,leth2) + s2).toCharArray();
+        }
+        for (int i = leth1 - 1; i >= 0 ; i--) {
+            res = Character.getNumericValue(c1[i]) +
+                    Character.getNumericValue(c2[i]) + temp;
+            String resTmp = "" + res ;
+            if (res >= 10){
+                temp = 1;  // 往高位进1
+                result.append(resTmp.charAt(1));
+            }else {
+                temp = 0;
+                result.append(resTmp);
+            }
+        }
+        if (temp == 0){
+            resultTmp = result.reverse().toString();
+        }else {
+            resultTmp = "1" + result.reverse().toString();
+        }
+        return resultTmp;
+    }
+
+    private static String getSb0(int leth1,int leth2){
+        StringBuffer sb0 = new StringBuffer();
+        if (leth1 >= leth2){
+            for (int i = 0; i < leth1 - leth2; i++) {
+                sb0.append("0");
+            }
+        }
+        return sb0.toString();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(addBySum("12923","123"));
+        System.out.println(addBySum2("199923","123"));
+
+
+    }
 }
