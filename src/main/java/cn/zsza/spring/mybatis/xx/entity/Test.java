6ff760fa1b341f7dd11292230023e8c214112110
zhangsong1 <zhangsong1@nash.work>
2017-10-27 09:54:37 +0800

    新增实体

---
+++ src/main/java/cn/zsza/spring/mybatis/xx/entity/Test.java
@@ -0,0 +1,12 @@
+package cn.zsza.spring.mybatis.xx.entity;
+
+import lombok.Data;
+
+/**
+ * Created by zhangsong on 2017/10/26.
+ */
+@Data
+public class Test {
+    private Integer id;
+    private String song;
+}
